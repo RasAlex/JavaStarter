5732eb050aece051ae56e185c41215827474b681
RasAlex <magvaj@gmail.com>
2018-12-07 12:23:06 +0200

    Initial commit for Home tasks

---
+++ src/Task02_02.java
@@ -0,0 +1,9 @@
+public class Task02_02 {
+    public static void main(String[] args) {
+        double pi=3.141592653;
+        double e=2.7182818284590452;
+
+        System.out.println("PI = " + pi);
+        System.out.println("E = "+ e);
+    }
+}
---
+++ src/Task02_03.java
@@ -0,0 +1,9 @@
+public class Task02_03 {
+    public static void main(String[] args) {
+        String s1 = "\nмоя строка 1";
+        String s2 = "\tмоя строка 2";
+        // string s3 = "\aмоя строка 3";
+        System.out.println(s1);
+        System.out.println((s2));
+    }
+}
